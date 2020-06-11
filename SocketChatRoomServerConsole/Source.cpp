2213af0a4b45e2aa32660285362370a68279c269
unknown <phuduy2610@gmail.com>
2020-06-11 15:32:56 +0700

    file server console

---
+++ SocketChatRoomServerConsole/Source.cpp
@@ -0,0 +1,25 @@
+﻿// EXAMPLE OF USAGE
+
+#include <iostream>
+#include <string>
+#include "Tcpserver.h"
+
+using namespace std;
+
+int main()
+{
+	string input;
+	TcpServer* server = new TcpServer("127.0.0.1", 54000, MessageReceived);
+
+	if (server->Init())
+	{
+		server->Run();
+	}
+	else
+	{
+		cout << "Fail to init server" << endl;
+	}
+	delete server;
+	cin.get();
+	return 0;
+}
