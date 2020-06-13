ab5da94355594074b751798fb756d5a4ac0d01bb
unknown <phuduy2610@gmail.com>
2020-06-13 11:26:54 +0700

    ver1

--- SocketChatRoomServer/SocketChatRoomServer/TcpServer.h
+++ SocketChatRoomServer/SocketChatRoomServer/TcpServer.h
@@ -8,50 +8,74 @@
 #include <map>
 #pragma comment (lib,"ws2_32.lib")
 
-class TcpServer;
 
-typedef void (*MessageRecievedHandler)(TcpServer* listener, SOCKET socketID, std::string msg); // con trỏ hàm 
-
+//class TcpServer;
+//
+//typedef void (*MessageRecievedHandler)(TcpServer* listener, SOCKET socketID, std::string msg); // con trỏ hàm 
+//
+//class TcpServer
+//{
+//public:
+//	std::string ipaddress;
+//	int port_number;
+//	sockaddr_in hint;
+//	bool thread_running; // điều khiển thread
+//	SOCKET listening; //socket để nghe tín hiệu
+//	
+//	fd_set master; //fd chứa các socket
+//
+//	MessageRecievedHandler MessageRecieved; 
+//
+//	//Tạo socket 
+//	SOCKET CreateSocket();	
+//
+//public:
+//	TcpServer(std::string IPaddress, int port_num, MessageRecievedHandler handler)
+//	{
+//		this->ipaddress = IPaddress;
+//		this->port_number = port_num;
+//		this->MessageRecieved = handler;
+//		FD_ZERO(&master); // clean cho set trống
+//		thread_running = false;
+//	}
+//
+//	~TcpServer();
+//
+//	bool Init();
+//
+//	//Hàm để gửi message đến 1 client nhất định
+//	bool Send(SOCKET socketclient, std::string message); 
+//	//Hàm gửi tin nhắn đến tất cả client trừ 1 người gửi ra ( thường là trừ người gửi tin nhắn đó )
+//	//Nếu dùng để gửi tin nhắn từ server đến tất cả client thì cho socketclient = NULL
+//	void SendAllClientExcept(SOCKET socketclient, std::string message);
+//	void Run();
+//	
+//};
+//
+//UINT Thread(LPVOID server);
+//
+//void MessageReceived(TcpServer* listen_server, SOCKET client, std::string msg);
+//
 class TcpServer
 {
-public:
-	std::string ipaddress;
-	int port_number;
-	sockaddr_in hint;
-	bool thread_running; // điều khiển thread
-	SOCKET listening; //socket để nghe tín hiệu
-	
-	fd_set master; //fd chứa các socket
+private:
+	std::vector<std::map<int, std::string>> _listUser; // Lưu đang on
+	SOCKET _listeningSocket;
+	int _port ;
+	std::string _ipAddress;
+	sockaddr_in _hint;
+	bool _isRunning;
 
-	MessageRecievedHandler MessageRecieved; 
-
-	//Tạo socket 
-	SOCKET CreateSocket();	
 
 public:
-	TcpServer(std::string IPaddress, int port_num, MessageRecievedHandler handler)
-	{
-		this->ipaddress = IPaddress;
-		this->port_number = port_num;
-		this->MessageRecieved = handler;
-		FD_ZERO(&master); // clean cho set trống
-		thread_running = false;
-	}
-
-	~TcpServer();
-
-	bool Init();
-
-	//Hàm để gửi message đến 1 client nhất định
-	bool Send(SOCKET socketclient, std::string message); 
-	//Hàm gửi tin nhắn đến tất cả client trừ 1 người gửi ra ( thường là trừ người gửi tin nhắn đó )
-	//Nếu dùng để gửi tin nhắn từ server đến tất cả client thì cho socketclient = NULL
-	void SendAllClientExcept(SOCKET socketclient, std::string message);
-	void Run();
-	
+	// Khởi tạo -> Run -> Listen -> receive -> analyzeAndProcess -> SendPacketRaw
+	TcpServer();
+	SOCKET CreateSocket(); //Tạo socket nghe
+	void SendPacketRaw(SOCKET clientSocket, std::string packet );
+	void AnalyzeAndProcess(std::string packet);
+	std::string ReceivePacket(SOCKET clientSocket);
+	bool Listen(); // Gọi receivePacket
+	void CloseServer();
+	bool IsValidUser(std::string username,std::string password);
+	void Run(); // Goị listen
 };
-
-UINT Thread(LPVOID server);
-
-void MessageReceived(TcpServer* listen_server, SOCKET client, std::string msg);
-
